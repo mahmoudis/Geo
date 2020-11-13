b789388d5f63efc4a47a3dad68b9d9adbf806dba
mahmoudis <said.mahmoudi@umons.ac.be>
2020-11-13 23:44:21 +0100

    first commit

---
+++ src/test/java/UMONS/AdditionTest.java
@@ -0,0 +1,29 @@
+package UMONS;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdditionTest {
+
+    @Test
+    public void add() {
+        System.out.println("Test True ");
+        assertTrue(Addition.add(5, 3) == 8);
+
+    }
+
+
+        @Test
+        public void addEquals() {
+            System.out.println("Test addEquals");
+            assertEquals(Addition.add(5, 3), 8);
+        }
+
+        @Test
+        public void addNotEquals() {
+            System.out.println("Test addNotEquals");
+            assertNotEquals(Addition.add(1, 1), 3);
+        }
+
+}
